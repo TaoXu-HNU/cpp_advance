85eb9e55af8845e3a24d3024ae98d0d45b5946c6
TaoXu-HNU <528858941@qq.com>
2023-05-26 18:58:28 +0800

    compile project

--- src/pointer.cpp
+++ src/pointer.cpp
@@ -1,1 +1,24 @@
-# include <include/pointer.hpp>
+# include <pointer.hpp>
+
+Pointer::Pointer()
+{
+
+}
+
+Pointer::~Pointer()
+{
+
+}
+
+void Pointer::pointer_Compute()
+{
+    int a = 0;
+    std::cout<<a<<std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    Pointer *pointer = new Pointer();
+
+    pointer->pointer_Compute();
+}
